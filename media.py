2ef0cb9d94622880c9fc674038187873f79ecc10
clarawisenfath <cx022007@gmail.com>
2024-11-20 10:51:55 -0300

    calculando a media ponderada

--- media.py
+++ media.py
@@ -2,13 +2,13 @@ nota1 = float(input("Digite a primeira nota: "))
 nota2 = float(input("Digite a segunda nota: "))
 nota3 = float(input("Digite a terceira nota: "))
 
-media = (nota1 + nota2 + nota3) / 3
+# Cálculo da média ponderada
+media = (nota1 * 1 + nota2 * 1 + nota3 * 2) / 4
 
 print(f"Sua média é: {media:.2f}")
-
 if media >= 6:
-    print("Você foi aprovado")
+   print("Você foi aprovado")
 elif 3 < media < 6:
     print("Você está de recuperação")
 else:
-    print("Você foi reprovado")
+   print("Você foi reprovado")
