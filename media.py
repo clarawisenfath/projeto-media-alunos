7db8095e3a292bd742b2358b18f9a2efe05f51e3
clarawisenfath <cx022007@gmail.com>
2024-11-20 09:49:03 -0300

    recuperação

--- media.py
+++ media.py
@@ -8,5 +8,7 @@ print(f"Sua média é: {media:.2f}")
 
 if media >= 6:
     print("Você foi aprovado")
+elif 3 < media < 6:
+    print("Você está de recuperação")
 else:
     print("Você foi reprovado")
